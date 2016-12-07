71cd1f353da52ac6de9a944f6fbc4c5a58320bf9
jmtrinidad <jm.trinidad.99@hotmail.com>
2016-12-07 15:13:40 -0400

    prueba commit

--- PuntoDeVenta/src/modelos/Producto.java
+++ PuntoDeVenta/src/modelos/Producto.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Producto implements IEntidadDatos<Producto> {
-	
+	//test Commit  to server
 	private int id;
 	private String codigo;
 	private String descripcion;
