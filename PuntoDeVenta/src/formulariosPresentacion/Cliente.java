07f1a85750d3bef49c97bd66da22cb508a3dae57
Jose Miguel <remigio9966@gmail.com>
2016-12-07 22:25:59 -0400

    Modificando Formulario Cliente

--- PuntoDeVenta/src/formulariosPresentacion/Cliente.java
+++ PuntoDeVenta/src/formulariosPresentacion/Cliente.java
@@ -1,6 +1,6 @@
 package formulariosPresentacion;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -17,6 +17,10 @@ public class Cliente extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JLabel lblSexo;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -38,8 +42,9 @@ public class Cliente extends JFrame {
 	 * Create the frame.
 	 */
 	public Cliente() {
+		setTitle("Cliente");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 556, 300);
+		setBounds(100, 100, 556, 217);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -56,16 +61,16 @@ public class Cliente extends JFrame {
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-		textField.setBounds(330, 33, 157, 20);
+		textField.setBounds(336, 33, 194, 20);
 		contentPane.add(textField);
 		
 		JLabel lblApellido = new JLabel("Apellido:");
-		lblApellido.setBounds(264, 36, 46, 14);
+		lblApellido.setBounds(264, 36, 62, 14);
 		contentPane.add(lblApellido);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(330, 61, 157, 20);
+		textField_1.setBounds(336, 61, 194, 20);
 		contentPane.add(textField_1);
 		
 		lblSexo = new JLabel("Sexo:");
@@ -81,7 +86,43 @@ public class Cliente extends JFrame {
 		contentPane.add(rdbtnMasculino);
 		
 		JLabel lblNewLabel = new JLabel("Cedula/RNC:");
-		lblNewLabel.setBounds(264, 64, 62, 14);
+		lblNewLabel.setBounds(255, 64, 71, 14);
 		contentPane.add(lblNewLabel);
+		
+		JLabel lblDireccion = new JLabel("Direccion:");
+		lblDireccion.setBounds(31, 136, 62, 14);
+		contentPane.add(lblDireccion);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(87, 133, 443, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblTelefono = new JLabel("Telefono");
+		lblTelefono.setBounds(31, 88, 46, 14);
+		contentPane.add(lblTelefono);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(87, 85, 167, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblCelular = new JLabel("Celular:");
+		lblCelular.setBounds(278, 88, 46, 14);
+		contentPane.add(lblCelular);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(336, 85, 194, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(31, 113, 46, 14);
+		contentPane.add(lblEmail);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(87, 110, 443, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
 	}
 }
