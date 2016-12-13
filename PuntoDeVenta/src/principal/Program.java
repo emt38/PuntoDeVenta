a92602cfb3ed89c0b91797c4b114d6e1ca1804e2
emt_t <emt_t@MI-AMADISIMA>
2016-12-13 16:19:07 -0400

    Program ejemplo comentado

--- PuntoDeVenta/src/principal/Program.java
+++ PuntoDeVenta/src/principal/Program.java
@@ -34,41 +34,41 @@ public class Program {
 		} catch (Exception e) {
 		    // If Nimbus is not available, you can set the GUI to another look and feel.
 		}
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MasterFrame window = new MasterFrame();
+					window.getFrame().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+//		List<Pais> paises = new Pais().listar();
 //		
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					MasterFrame window = new MasterFrame();
-//					window.getFrame().setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-		
-		List<Pais> paises = new Pais().listar();
-		
-		for(Pais p : paises) {
-			System.out.println(p.getNombre());
-		}
-		
-		System.out.println();
-		
-		List<Provincia> provincias = new Provincia().listar();
-		
-		for(Provincia p : provincias) {
-			System.out.println(p.getNombre());
-		}
-		
-		System.out.println();
-		
-		Ciudad temp = new Ciudad(0, provincias.get(0), "San Francisco de Macorís");
-		//temp.insertar();
-		
-		List<Ciudad> ciudades = new Ciudad().listar();
-		
-		for(Ciudad c : ciudades) {
-			System.out.println(c.getNombre());
-		}
+//		for(Pais p : paises) {
+//			System.out.println(p.getNombre());
+//		}
+//		
+//		System.out.println();
+//		
+//		List<Provincia> provincias = new Provincia().listar();
+//		
+//		for(Provincia p : provincias) {
+//			System.out.println(p.getNombre());
+//		}
+//		
+//		System.out.println();
+//		
+//		Ciudad temp = new Ciudad(0, provincias.get(0), "San Francisco de Macorís");
+//		//temp.insertar();
+//		
+//		List<Ciudad> ciudades = new Ciudad().listar();
+//		
+//		for(Ciudad c : ciudades) {
+//			System.out.println(c.getNombre());
+//		}
 	}
 }
