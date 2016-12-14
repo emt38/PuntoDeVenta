c306a987dc98b6bcd9abe09273d61fd7c570f010
franklyn <franklyn@Soporte04.oncologico.local>
2016-12-14 14:08:57 -0400

    NotaCredito y NotaDebito

--- PuntoDeVenta/src/modelos/NotaDebito.java
+++ PuntoDeVenta/src/modelos/NotaDebito.java
@@ -1,6 +1,10 @@
 package modelos;
 
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+
+import principal.Utilidades;
 
 public class NotaDebito extends AcuerdoComercial implements IEntidadDatos<modelos.AcuerdoComercial> {
 	
@@ -36,18 +40,44 @@ public class NotaDebito extends AcuerdoComercial implements IEntidadDatos<modelo
 	@Override
 	public boolean insertar() {
 		// TODO Auto-generated method stub
+		HashMap<String, Object> temp = new HashMap<>();
+		temp.put("_idSuplidor", suplidor.getId());
+		temp.put("_concepto", concepto);
+		
+		try (Connection gate = Utilidades.newConnection();) {
+			return Utilidades.ejecutarCall("CALL AgregarNotaDebito(?,?)", temp, gate);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean actualizar() {
 		// TODO Auto-generated method stub
+		HashMap<String, Object> temp = new HashMap<>();
+		temp.put("_idSuplidor", suplidor);
+		temp.put("_concepto", concepto);
+		
+		try (Connection gate = Utilidades.newConnection();) {
+			return Utilidades.ejecutarCall("CALL ModificarNotaDebito(?,?,?)", temp, gate);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean eliminar() {
 		// TODO Auto-generated method stub
+		HashMap<String, Object> temp = new HashMap<>();
+		temp.put("_idSuplidor", suplidor);
+		
+		try (Connection gate = Utilidades.newConnection();) {
+			return Utilidades.ejecutarCall("CALL EliminarNotaDebito(?)", temp, gate);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		return false;
 	}
 
