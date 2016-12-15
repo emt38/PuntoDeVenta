facf7fa240fe20d98ec7d55efed5bc300da91019
FRANKLYN DE JESUS <FRANKLYN DE JESUS@DESKTOP-6QNJAF5>
2016-12-15 01:57:59 -0400

    Nota debito y nota credito

---
+++ PuntoDeVenta/src/modelos/NotaDebito.java
@@ -0,0 +1,92 @@
+package modelos;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import principal.Utilidades;
+
+public class NotaDebito extends AcuerdoComercial implements IEntidadDatos<NotaDebito> {
+	
+	private int idNotaDebito;
+	private Date fecha;
+	private float total;
+	private float efectuado;
+	private Suplidor suplidor;
+	private String concepto;
+	
+	public NotaDebito() {
+		super();
+	}
+
+	public NotaDebito(int idNotaDebito, Date fecha, float total, float efectuado, Suplidor suplidor, String concepto) {
+		super();
+		this.idNotaDebito = idNotaDebito;
+		this.fecha = fecha;
+		this.total = total;
+		this.efectuado = efectuado;
+		this.suplidor = suplidor;
+		this.concepto = concepto;
+	}
+
+	@Override
+	public boolean insertar() {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> temp = new HashMap<>();
+		temp.put("_idNotaCredito", idNotaDebito);
+		temp.put("_fecha", fecha);
+		temp.put("_total", total);
+		temp.put("_efectuado", efectuado);
+		temp.put("_supidor", suplidor.getId());
+		temp.put("_concepto", concepto);
+		
+		try (Connection gate = Utilidades.newConnection();) {
+			return Utilidades.ejecutarCall("CALL AgregarNotaCredito(?,?)", temp, gate);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean actualizar() {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> temp = new HashMap<>();
+		temp.put("_idNotaCredito", idNotaDebito);
+		temp.put("_fecha", fecha);
+		temp.put("_total", total);
+		temp.put("_efectuado", efectuado);
+		temp.put("_siplidor", suplidor.getId());
+		temp.put("_concepto", concepto);
+		
+		try (Connection gate = Utilidades.newConnection();) {
+			return Utilidades.ejecutarCall("CALL ModificarNotaCredito(?,?,?)", temp, gate);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean eliminar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public NotaDebito buscar(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<NotaDebito> listar(String textoBusqueda) {
+		// Mi primer commit, ahora mira cómo
+		List<NotaDebito> notasDebito = new ArrayList<>();
+		return null;
+	}
+
+}
