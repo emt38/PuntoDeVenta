d8e7bbcb2eab9c6276b92c5f6c21ff841fa3f76d
Erick <Erick@Erick-PC>
2016-12-21 00:23:36 -0300

    jh

--- PuntoDeVenta/src/vistas/TiendasFrame.java
+++ PuntoDeVenta/src/vistas/TiendasFrame.java
@@ -24,20 +24,43 @@ public class TiendasFrame extends JFrame{
 
 	private JFrame frame;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNewLabel;
+	private JTextField textField_1;
 	
 
 	
 	public TiendasFrame() {
 		setTitle("Tiendas");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 300, 250);
+		setBounds(100, 100, 574, 288);
+		
 		contentPane = new JPanel();
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel lblID = new JLabel("New label");
+		lblID.setBounds(10, 11, 46, 14);
+		lblID.setText("ID");
+		contentPane.add(lblID);
+		
+		JTextField tfID = new JTextField();
+		tfID.setBounds(31, 11, 46, 20);
+		contentPane.add(tfID);
+		tfID.setColumns(10);
+		
+		JLabel lblNombre = new JLabel("New label");
+		lblNombre.setBounds(10, 36, 46, 14);
+		lblNombre.setText("Nombre");
+		contentPane.add(lblNombre);
+		
+		JTextField jtfNombre = new JTextField();
+		jtfNombre.setBounds(54, 36, 86, 20);
+		contentPane.add(jtfNombre);
+		jtfNombre.setColumns(10);
+		
 		
 	}
-
-
 }
