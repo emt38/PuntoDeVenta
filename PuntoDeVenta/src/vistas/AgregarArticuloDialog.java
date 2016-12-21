7aef6482a998bedcfdb0dd8b9c805a6aa2f7b21e
Fran <Fran@192.168.1.6>
2016-12-21 13:44:50 -0400

    AgregarArticuloDialog evia una lista de articulos

--- PuntoDeVenta/src/vistas/AgregarArticuloDialog.java
+++ PuntoDeVenta/src/vistas/AgregarArticuloDialog.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,18 +15,25 @@ import javax.swing.table.DefaultTableModel;
 import modelos.Articulo;
 import modelos.Compra;
 import modelos.Producto;
+import modelos.Suplidor;
 import principal.Utilidades;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.JTable;
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import java.awt.Component;
 import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
 
 public class AgregarArticuloDialog extends JDialog{
 
@@ -34,6 +42,7 @@ public class AgregarArticuloDialog extends JDialog{
 	private JScrollPane scrollPane;
 	private JTextField txtBusqueda;
 	private Compra compra = new Compra();
+	List<Articulo> articulos = compra.getArticulos();
 	
 	public boolean articuloAgregado = false;
 	private JTextField textCantidad;
@@ -74,7 +83,7 @@ public class AgregarArticuloDialog extends JDialog{
 		tabla = new JTable();
 		String[] columnas = {"Cantidad", "Producto", "Costo", "Impuestos", "Subtotal"};
 		String[] campos = {"cantidad", "producto","valor", "impuestos", "subtotal"};  
-		Object[][] datos = Utilidades.listToBidiArray(compra.getArticulos(), campos);
+		Object[][] datos = Utilidades.listToBidiArray(articulos, campos);
 		
 		DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
 		//Para evitar que las celdas sean editables
@@ -93,26 +102,25 @@ public class AgregarArticuloDialog extends JDialog{
 		scrollPane.setBounds(10, 109, 536, 112);
 		contentPane.add(scrollPane);
 		
-		String textoBusqueda = txtBusqueda.getText().replace("'","''").replace("\"", "\"\"");
-		Producto producto = new Producto(); 
+		String textoBusqueda = txtBusqueda.getText().replace("'","''").replace("\"", "\"\""); 
 		Articulo articulo = new Articulo();
 		
 		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(229, 33, 89, 23);
+		btnBuscar.setBounds(418, 27, 101, 39);
 		contentPane.add(btnBuscar);
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				List<Producto> productos = producto.listar();
+				Producto producto = new Producto();
+				List<Producto> productos = new Producto().listar();
 				
 				if(cbbxMetodoBusqueda.getSelectedItem() == "Id"){
 					if(CompraFrame.isInt(txtBusqueda.getText())){
-						productos = producto.listar("WHERE p.idproducto LIKE " + txtBusqueda.getText());
+						productos = new Producto().listar("WHERE p.idproducto = " + txtBusqueda.getText());
 					}
 					else
 						return;
 				}else if(cbbxMetodoBusqueda.getSelectedItem() == "Descripcion"){
-					productos = producto.listar("WHERE descripcion LIKE '%" + txtBusqueda.getText() + "%' OR codigo LIKE  '%" + txtBusqueda.getText() + "%' ");
+					productos = new Producto().listar("WHERE descripcion LIKE '%" + txtBusqueda.getText() + "%' OR codigo LIKE  '%" + txtBusqueda.getText() + "%' ");
 				}
 				
 				if (productos.size()==0){
@@ -128,21 +136,32 @@ public class AgregarArticuloDialog extends JDialog{
 				producto.setPrecio(productos.get(0).getPrecio());
 				producto.setTasaImpuesto(productos.get(0).getTasaImpuesto());
 				
-				articulo.setCantidad(1);
-				articulo.setProducto(producto);
-				articulo.setValor(producto.getPrecio());
-				articulo.setTasaImpuestos(producto.getTasaImpuesto());
+				Articulo articuloTemp = new Articulo();
+				
+				if (CompraFrame.isInt(textCantidad.getText()))
+					articuloTemp.setCantidad(Integer.parseInt(textCantidad.getText()));
+				else
+					articuloTemp.setCantidad(1);
+				articuloTemp.setProducto(producto);
+				articuloTemp.setValor(producto.getPrecio());
+				articuloTemp.setTasaImpuestos(producto.getTasaImpuesto());
+				articuloTemp.totalizar();
+				
+				articulo.setCantidad(articuloTemp.getCantidad());
+				articulo.setProducto(articuloTemp.getProducto());
+				articulo.setValor(articuloTemp.getProducto().getPrecio());
+				articulo.setTasaImpuestos(articuloTemp.getProducto().getTasaImpuesto());
 				articulo.totalizar();
+
 				
-				List<Articulo> articulos = new ArrayList<Articulo>();
-				articulos.add(articulo);
+				articulos.add(articuloTemp);
 				
-				String[] columnas = {"Producto", "Costo", "Impuestos", "Subtotal"};
-				String[] campos = {"producto","valor", "impuestos", "subtotal"};  
+				String[] columnas = {"Cantidad", "Producto", "Costo", "Impuestos", "Subtotal"};
+				String[] campos = {"cantidad","producto","valor", "impuestos", "subtotal"};  
 				Object[][] datos = Utilidades.listToBidiArray(articulos, campos);
 				
 				for(int i = 0; i< datos.length; i++){
-					datos[i][0] = ((Producto) datos[i][0]).getDescripcion(); 
+					datos[i][1] = ((Producto) datos[i][1]).getDescripcion(); 
 				}
 				
 				DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
@@ -166,14 +185,8 @@ public class AgregarArticuloDialog extends JDialog{
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try{
-					if (CompraFrame.isInt(textCantidad.getText()))
-						articulo.setCantidad(Integer.parseInt(textCantidad.getText()));
-					else
-						articulo.setCantidad(1);
 					
-					articulo.totalizar();
-					
-					pasarObjeto(articulo);
+					pasarObjeto(articulos);
 					articuloAgregado = true;
 				}
 				catch(Exception e){
@@ -190,13 +203,17 @@ public class AgregarArticuloDialog extends JDialog{
 		
 		JLabel lblCantidad = new JLabel("Cantidad");
 		lblCantidad.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblCantidad.setBounds(35, 67, 86, 14);
+		lblCantidad.setBounds(227, 38, 55, 14);
 		contentPane.add(lblCantidad);
 		
-		textCantidad = new JTextField();
-		textCantidad.setBounds(131, 67, 86, 20);
+		textCantidad = new JTextField("1");
+		textCantidad.setBounds(292, 36, 86, 20);
 		contentPane.add(textCantidad);
 		textCantidad.setColumns(10);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 78, 536, 2);
+		contentPane.add(separator);
 		
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -204,8 +221,29 @@ public class AgregarArticuloDialog extends JDialog{
 				AgregarArticuloDialog.this.dispose();
 			}
 		});
-
 		
+		
+		
+		//////Para eliminar articulos///////
+			  int condition = JComponent.WHEN_IN_FOCUSED_WINDOW;
+			  InputMap inputMap = tabla.getInputMap(condition);
+			  ActionMap actionMap = tabla.getActionMap();
+			  
+			  final String DELETE = "Delete";
+
+			  inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0), DELETE);
+			  actionMap.put(DELETE, new AbstractAction() {
+			     public void actionPerformed(ActionEvent e) {
+			    	 
+			    	 String productoDescripcion = (String) tabla.getValueAt(tabla.getSelectedRow(), 1);
+			 		
+			 		for(int i = 0; i< articulos.size(); i++){
+			 			if (productoDescripcion == articulos.get(i).getProducto().getDescripcion())
+			 				articulos.remove(i);
+			 		}
+			 		ReloadAll();
+			     }
+			  });
 	}
 	
 	static Object objeto;
@@ -217,7 +255,28 @@ public class AgregarArticuloDialog extends JDialog{
 	public static Object pasarObjeto(){
 		return objeto;
 	}
-
+	
+	private void ReloadAll() {
+		String[] columnas = {"Cantidad", "Producto", "Costo", "Impuestos", "Subtotal"};
+		String[] campos = {"cantidad", "producto","valor", "impuestos", "subtotal"};  
+		Object[][] datos = Utilidades.listToBidiArray(articulos, campos);
+		
+		for(int i = 0; i< datos.length; i++){
+			datos[i][1] = ((Producto) datos[i][1]).getDescripcion(); 
+		}
+		
+		DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
+		//Para evitar que las celdas sean editables
+		{
+		    @Override
+		    public boolean isCellEditable (int fila, int columna) {
+		        return false;
+		    }
+		};
+		tabla.setModel(modelo);
+		tabla.getColumnModel().getColumn(0).setPreferredWidth(76);
+		tabla.getColumnModel().getColumn(1).setPreferredWidth(323);
+	}
 	
 	/**
 	 * Launch the application.
