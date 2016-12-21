b0436c4a8c5b07e6f2257a7c931a6011675fda0f
emt_t <emt_t@MI-AMADISIMA>
2016-12-21 00:35:09 -0400

    Cambios de diseño

--- PuntoDeVenta/src/vistas/CompraFrame.java
+++ PuntoDeVenta/src/vistas/CompraFrame.java
@@ -75,7 +75,7 @@ public class CompraFrame extends JFrame {
 	
 	public CompraFrame() {
 		setTitle("Compra");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 572, 376);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- PuntoDeVenta/src/vistas/ProductoFrame.java
+++ PuntoDeVenta/src/vistas/ProductoFrame.java
@@ -223,7 +223,7 @@ public class ProductoFrame extends JFrame implements WindowFocusListener {
 	}
 
 	public ProductoFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 568, 243);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
