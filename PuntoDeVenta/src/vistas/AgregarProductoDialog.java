1be9156dc481faee69bce7e81314a88f7ac2b2e3
Fran <Fran@FranLap>
2017-06-13 17:14:02 -0400

    Arreglando a VentaFrame y agregando AgregarProductoDialog

---
+++ PuntoDeVenta/src/vistas/AgregarProductoDialog.java
@@ -0,0 +1,341 @@
+package vistas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import modelos.Articulo;
+import modelos.Compra;
+import modelos.Producto;
+import modelos.Suplidor;
+import principal.Utilidades;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.JTable;
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.Component;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+
+public class AgregarProductoDialog extends JDialog{
+
+	private JPanel contentPane;
+	private JTable tabla;
+	private JTable tablaExterna;
+	private JScrollPane scrollPane;
+	private JTextField txtBusqueda;
+	List<Producto> productosJtable = new Producto().listar();
+	List<Producto>  productos = new ArrayList<Producto>();
+	static List<Articulo> articulos =  new ArrayList<Articulo>();
+	public boolean productoAgregado = false;
+	
+	
+	static Object objeto;
+	public static void pasarProducto(Object object){
+		objeto = object;
+	}
+	public static Object pasarProducto(){
+		return objeto;
+	}
+	
+	public static List<Articulo> getArticulosSeleccionados(){
+		return articulos;
+	}
+	public static void vaciarArticulosSeleccionados(){
+		articulos.clear();
+	}
+	
+	public AgregarProductoDialog(JTable tablaExterna) {
+		setTitle("Agregar articulo");
+		contentPane = new JPanel();
+		contentPane.setLayout(null);
+		setBounds(100, 100, 570, 401);
+		getContentPane().setLayout(new BorderLayout());
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPane, BorderLayout.CENTER);
+		
+		this.tablaExterna = tablaExterna;
+		
+		JLabel lblMetodoBusqueda = new JLabel("Metodo de busqueda: ");
+		lblMetodoBusqueda.setBounds(10, 11, 144, 14);
+		contentPane.add(lblMetodoBusqueda);
+		
+		JLabel lblResultados = new JLabel("Resultados");
+		lblResultados.setBounds(10, 91, 86, 16);
+		contentPane.add(lblResultados);
+		
+		List<String> metodosBusqueda = new ArrayList<>();
+		metodosBusqueda.add("Id");
+		metodosBusqueda.add("Descripcion");
+		
+		JComboBox cbbxMetodoBusqueda = new JComboBox(metodosBusqueda.toArray());
+		lblMetodoBusqueda.setLabelFor(cbbxMetodoBusqueda);
+		cbbxMetodoBusqueda.setBounds(10, 34, 111, 23);
+		contentPane.add(cbbxMetodoBusqueda);
+		
+		txtBusqueda = new JTextField();
+		txtBusqueda.setBounds(131, 34, 86, 25);
+		contentPane.add(txtBusqueda);
+		txtBusqueda.setColumns(10);
+		
+		contentPane.setLayout(null);
+		
+		tabla = new JTable();
+		String[] columnas = {"ID", "Codigo", "Descripcion", "Costo", "Precio", "Tasa de impuesto"};
+		String[] campos = {"Id", "Codigo", "Descripcion", "Costo", "Precio", "TasaImpuesto"};  
+		Object[][] datos = Utilidades.listToBidiArray(productosJtable, campos);
+		
+		DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
+		//Para evitar que las celdas sean editables
+		{
+		    @Override
+		    public boolean isCellEditable (int fila, int columna) {
+		        return false;
+		    }
+		};
+		
+		tabla.setModel(modelo);
+
+		tabla.getColumnModel().getColumn(0).setPreferredWidth(76);
+		tabla.getColumnModel().getColumn(1).setPreferredWidth(323);
+		scrollPane = new JScrollPane(tabla);
+		scrollPane.setBounds(10, 109, 536, 209);
+		contentPane.add(scrollPane);
+		
+		String textoBusqueda = txtBusqueda.getText().replace("'","''").replace("\"", "\"\""); 
+		Articulo articulo = new Articulo();
+		
+		JButton btnFiltrar = new JButton("Filtrar");
+		btnFiltrar.setBounds(418, 27, 101, 39);
+		contentPane.add(btnFiltrar);
+		getRootPane().setDefaultButton(btnFiltrar);
+		btnFiltrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				List<Producto> tempProductos = new Producto().listar();
+					
+				if(!(txtBusqueda.getText() == "")){
+					if(cbbxMetodoBusqueda.getSelectedItem() == "Id"){
+						if(CompraFrame.isInt(txtBusqueda.getText())){
+							tempProductos = new Producto().listar("WHERE p.idproducto = " + txtBusqueda.getText());
+						}
+					}else if(cbbxMetodoBusqueda.getSelectedItem() == "Descripcion"){
+						tempProductos = new Producto().listar("WHERE descripcion LIKE '%" + txtBusqueda.getText() + "%' OR codigo LIKE  '%" + txtBusqueda.getText() + "%' ");
+					}
+				}
+				else{
+					tempProductos = new Producto().listar();
+				}
+				
+				if (tempProductos.size()==0){
+					return;
+				}
+				
+				productos = tempProductos;
+				productosJtable = tempProductos;
+				
+				String[] columnas = {"ID", "Codigo", "Descripcion", "Costo", "Precio", "Tasa de impuesto"};
+				String[] campos = {"Id", "Codigo", "Descripcion", "Costo", "Precio", "TasaImpuesto"};  
+				Object[][] datos = Utilidades.listToBidiArray(productosJtable, campos);
+				
+				DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
+				//Para evitar que las celdas sean editables
+				{
+				    @Override
+				    public boolean isCellEditable (int fila, int columna) {
+				        return false;
+				    }
+				};
+				
+				tabla.setModel(modelo);
+			}
+		});
+		
+		tabla.setModel(modelo);
+		
+		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.setBounds(354, 329, 89, 23);
+		contentPane.add(btnAgregar);
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try{
+					int rowNum = tabla.getSelectedRow();
+					
+					Producto producto = ConvertRowToProduct(tabla, rowNum);
+					pasarProducto(producto);
+					ActualizarTablaExterna(tablaExterna);
+					
+					productoAgregado = true;
+				}
+				catch(Exception e){
+					productoAgregado = false;
+				}
+				
+			}
+		});
+		
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 78, 536, 2);
+		contentPane.add(separator);
+		
+		JButton btnCerrar = new JButton("Cerrar");
+		btnCerrar.setBounds(455, 329, 89, 23);
+		contentPane.add(btnCerrar);
+		btnCerrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnCerrar.setActionCommand("cancelar");
+				AgregarProductoDialog.this.dispose();
+			}
+		});
+		
+		
+		
+		//////Para eliminar productos///////
+			  int condition = JComponent.WHEN_IN_FOCUSED_WINDOW;
+			  InputMap inputMap = tabla.getInputMap(condition);
+			  ActionMap actionMap = tabla.getActionMap();
+			  
+			  final String DELETE = "Delete";
+
+			  inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0), DELETE);
+			  actionMap.put(DELETE, new AbstractAction() {
+			     public void actionPerformed(ActionEvent e) {
+			    	 
+			    	 String productoDescripcion = (String) tabla.getValueAt(tabla.getSelectedRow(), 1);
+			 		
+			 		for(int i = 0; i< productos.size(); i++){
+			 			if (productoDescripcion == productos.get(i).getDescripcion())
+			 				productos.remove(i);
+			 		}
+			 		ReloadAll();
+			     }
+			  });
+	}
+	
+	private Producto ConvertRowToProduct(JTable tabla, int rowNumber) {
+		Producto tempProducto = new Producto();    
+		
+		int colId = getColumnIndex(tabla, "ID");
+		int colCodigo = getColumnIndex(tabla, "Codigo");
+		int colDescripcion = getColumnIndex(tabla, "Descripcion");
+		int colCosto = getColumnIndex(tabla, "Costo");
+		int colPrecio = getColumnIndex(tabla, "Precio");
+		int colTasaImpuesto = getColumnIndex(tabla, "Tasa de impuesto");
+		
+		int Id = (int)tabla.getValueAt(rowNumber, colId);
+		String Codigo = (String)tabla.getValueAt(rowNumber, colCodigo);
+		String Descripcion = (String)tabla.getValueAt(rowNumber, colDescripcion);
+		float Costo = (float)tabla.getValueAt(rowNumber, colCosto);
+		float Precio = (float)tabla.getValueAt(rowNumber, colPrecio);
+		float TasaImpuesto = (float)tabla.getValueAt(rowNumber, colTasaImpuesto);
+		
+		tempProducto.setId(Id);
+		tempProducto.setCodigo(Codigo);
+		tempProducto.setDescripcion(Descripcion);
+		tempProducto.setCosto(Costo);
+		tempProducto.setPrecio(Precio);
+		tempProducto.setTasaImpuesto(TasaImpuesto);
+				
+		return tempProducto;
+	}
+	
+	private int getColumnIndex (JTable table, String colName) {
+	    for (int i=0; i < table.getColumnCount(); i++) {
+	        if (table.getColumnName(i).equals(colName)) return i;
+	    }
+	    return -1;
+	}
+	
+	public void ActualizarTablaExterna(JTable tabla) {
+		
+		int rowNum = this.tabla.getSelectedRow();
+		
+		Producto producto = ConvertRowToProduct(this.tabla, rowNum);
+		
+		Articulo articuloTemp = new Articulo();
+		articuloTemp.setCantidad(1);
+		articuloTemp.setProducto(producto);
+		articuloTemp.setValor(producto.getPrecio());
+		articuloTemp.setTasaImpuestos(producto.getTasaImpuesto());
+		articuloTemp.totalizar();
+				
+		articulos.add(articuloTemp);
+		
+		String[] columnas = {"Cantidad", "Producto", "Costo", "Impuestos", "Subtotal"};
+		String[] campos = {"cantidad", "producto","valor", "impuestos", "subtotal"};  
+		Object[][] datos = Utilidades.listToBidiArray(articulos, campos);
+		
+		for(int i = 0; i< datos.length; i++){
+			datos[i][1] = ((Producto) datos[i][1]).getDescripcion(); 
+		}
+		
+		DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
+		//Para evitar que las celdas sean editables
+		{
+		    @Override
+		    public boolean isCellEditable (int fila, int columna) {
+		        return false;
+		    }
+		};
+		tabla.setModel(modelo);
+		tabla.getColumnModel().getColumn(0).setPreferredWidth(76);
+		tabla.getColumnModel().getColumn(1).setPreferredWidth(323);
+	}
+	
+	private void ReloadAll() {
+		String[] columnas = {"ID", "Codigo", "Descripcion", "Costo", "Precio", "Tasa de impuesto"};
+		String[] campos = {"Id", "Codigo", "Descripcion", "Costo", "Precio", "TasaImpuesto"};    
+		Object[][] datos = Utilidades.listToBidiArray(productosJtable, campos);
+		
+		for(int i = 0; i< datos.length; i++){
+			datos[i][1] = ((Producto) datos[i][1]).getDescripcion(); 
+		}
+		
+		DefaultTableModel modelo = new DefaultTableModel(datos, columnas)
+				//Para evitar que las celdas sean editables
+				{
+			@Override
+			public boolean isCellEditable (int fila, int columna) {
+				return false;
+			}
+				};
+				tabla.setModel(modelo);
+				tabla.getColumnModel().getColumn(0).setPreferredWidth(76);
+				tabla.getColumnModel().getColumn(1).setPreferredWidth(323);
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AgregarProductoDialog dialog = new AgregarProductoDialog( new JTable());
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
--- PuntoDeVenta/src/vistas/VentaFrame.java
+++ PuntoDeVenta/src/vistas/VentaFrame.java
@@ -48,14 +48,16 @@ public class VentaFrame extends JFrame {
 	private JTextField txtCambiardesc = new JTextField();
 	private JTextField txtEfectivoRecibido = new JTextField();
 	private JTextField txtCambioDevuelto = new JTextField();
-	
 	private Venta venta = new Venta();
+	
+	String[] columnas = {"Cantidad", "Producto", "Costo", "Impuestos", "Subtotal"};
 	
 	Cliente cliente = new Cliente();
 	List<Cliente> clientes = new Cliente().listar("ORDER BY Nombre");
 	JComboBox<ComboItem> cbbxClientes = new JComboBox<ComboItem>();
 	private JLabel lblCliente;
 	private JLabel lblNota;
+	private JTextField txtAgregar;
 	
 	private void ReloadAll() {
 		
@@ -68,10 +70,8 @@ public class VentaFrame extends JFrame {
 			Object objeto = ((ComboItem)item).getObjeto();
 			cliente = (Cliente) objeto;
 		}
-		
-		String[] columnas = {"Cantidad", "Producto", "Costo", "Impuestos", "Subtotal"};
 		String[] campos = {"cantidad", "producto","valor", "impuestos", "subtotal"};  
-		Object[][] datos = Utilidades.listToBidiArray(venta.getArticulos(), campos);
+		Object[][] datos = Utilidades.listToBidiArray(AgregarProductoDialog.getArticulosSeleccionados(), campos);
 		
 		for(int i = 0; i< datos.length; i++){
 			datos[i][1] = ((Producto) datos[i][1]).getDescripcion(); 
@@ -100,7 +100,7 @@ public class VentaFrame extends JFrame {
 		setTitle("Venta");
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 572, 536);
+		setBounds(100, 100, 654, 536);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -118,42 +118,42 @@ public class VentaFrame extends JFrame {
 		tabla.getColumnModel().getColumn(0).setPreferredWidth(76);
 		tabla.getColumnModel().getColumn(1).setPreferredWidth(323);
 		scrollPane = new JScrollPane(tabla);
-		scrollPane.setBounds(10, 86, 536, 227);
+		scrollPane.setBounds(10, 86, 618, 227);
 		contentPane.add(scrollPane);
 		
 		ReloadAll();
 		
-		cbbxClientes.setBounds(379, 11, 167, 25);
+		cbbxClientes.setBounds(458, 11, 167, 25);
 		contentPane.add(cbbxClientes);
 		
 		lblTotalImpuesto = new JLabel("Total impuesto: ");
-		lblTotalImpuesto.setBounds(346, 357, 104, 14);
+		lblTotalImpuesto.setBounds(402, 357, 130, 14);
 		contentPane.add(lblTotalImpuesto);
 		
 		lblTotalDescuento = new JLabel("Total descuento: ");
-		lblTotalDescuento.setBounds(346, 392, 104, 14);
+		lblTotalDescuento.setBounds(402, 392, 130, 14);
 		contentPane.add(lblTotalDescuento);
 		
 		lblTotal = new JLabel("Total: ");
-		lblTotal.setBounds(346, 428, 104, 14);
+		lblTotal.setBounds(402, 428, 130, 14);
 		contentPane.add(lblTotal);
 		
 		lblEfectivoRecibido = new JLabel("Efectivo recibido: ");
-		lblEfectivoRecibido.setBounds(10, 324, 86, 14);
+		lblEfectivoRecibido.setBounds(10, 324, 118, 14);
 		contentPane.add(lblEfectivoRecibido);
 		
 		lblCambioDevuelto = new JLabel("Cambio devuelto: ");
-		lblCambioDevuelto.setBounds(10, 357, 96, 14);
+		lblCambioDevuelto.setBounds(10, 357, 118, 14);
 		contentPane.add(lblCambioDevuelto);
 		
 		txtCambioDevuelto = new JTextField();
 		txtCambioDevuelto.setEditable(false);
-		txtCambioDevuelto.setBounds(106, 352, 86, 25);
+		txtCambioDevuelto.setBounds(138, 352, 86, 25);
 		contentPane.add(txtCambioDevuelto);
 		txtCambioDevuelto.setColumns(10);
 		
 		txtEfectivoRecibido = new JTextField();
-		txtEfectivoRecibido.setBounds(106, 321, 86, 25);
+		txtEfectivoRecibido.setBounds(138, 321, 86, 25);
 		contentPane.add(txtEfectivoRecibido);
 		txtEfectivoRecibido.setColumns(10);
 		//Para tomar el Enter
@@ -164,75 +164,84 @@ public class VentaFrame extends JFrame {
 		    {
 		        if(isFloat(txtEfectivoRecibido.getText())){
 		        	venta.setEfectivoRecibido(new Float(Float.parseFloat(txtEfectivoRecibido.getText().replaceAll(",", "."))).intValue());
-		        	venta.setCambioDevuelto(new Float(venta.getEfectivoRecibido() - venta.getTotal()).intValue());
-		        	txtCambioDevuelto.setText(Integer.toString(venta.getCambioDevuelto()));
+		        	if((venta.getEfectivoRecibido())>(venta.getTotal())){
+		        		venta.setCambioDevuelto(new Float(venta.getEfectivoRecibido() - venta.getTotal()).intValue());
+		        		txtCambioDevuelto.setText(Integer.toString(venta.getCambioDevuelto()));		        		
+		        	}
+		        	else
+		        		txtCambioDevuelto.setText("0");
+		        		
 		        }
 		    }
 		});
 		
 		txtTotalImpuestos = new JTextField();
-		txtTotalImpuestos.setBounds(460, 352, 86, 25);
+		txtTotalImpuestos.setBounds(542, 352, 86, 25);
 		contentPane.add(txtTotalImpuestos);
 		txtTotalImpuestos.setColumns(10);
 		txtTotalImpuestos.setEditable(false);
 		txtTotalImpuestos.setText(String.format("%.2f", venta.getImpuestos()));
 		
 		txtTotalDescuento = new JTextField();
-		txtTotalDescuento.setBounds(460, 387, 86, 25);
+		txtTotalDescuento.setBounds(542, 387, 86, 25);
 		contentPane.add(txtTotalDescuento);
 		txtTotalDescuento.setColumns(10);
 		txtTotalDescuento.setEditable(false);
 		txtTotalDescuento.setText(String.format("%.2f", venta.getDescuentos()));
 		
 		txtTotal = new JTextField();
-		txtTotal.setBounds(460, 423, 86, 25);
+		txtTotal.setBounds(542, 423, 86, 25);
 		contentPane.add(txtTotal);
 		txtTotal.setColumns(10);
 		txtTotal.setEditable(false);
 		txtTotal.setText(String.format("%.2f", venta.getTotal()));
 		
-		JButton btnAgregarArticulo = new JButton("Agregar Articulo");
-		btnAgregarArticulo.addActionListener(new ActionListener() {
+		JButton btnBuscarArticulos = new JButton("Buscar Articulos");
+		btnBuscarArticulos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				AgregarArticuloDialog agregarArticulo = new AgregarArticuloDialog();
-				agregarArticulo.setModal(true);
-				agregarArticulo.setVisible(true);
-				agregarArticulo.setResizable(false);
-				
-				if(agregarArticulo.articuloAgregado){
-					for(Articulo artic: (List<Articulo>)(AgregarArticuloDialog.pasarObjeto())){
+				AgregarProductoDialog agregarProducto = new AgregarProductoDialog(tabla);
+				agregarProducto.setModal(true);
+				agregarProducto.setVisible(true);
+				agregarProducto.setResizable(false);
+								
+				if(AgregarProductoDialog.getArticulosSeleccionados().size()>0){
+					for(Articulo artic: AgregarProductoDialog.getArticulosSeleccionados()){
 						venta.agregarArticulo(artic);
 					}
 					ReloadAll();
 				}
 			}
 		});
-		btnAgregarArticulo.setBounds(10, 52, 148, 23);
-		contentPane.add(btnAgregarArticulo);
+		btnBuscarArticulos.setBounds(10, 11, 148, 23);
+		contentPane.add(btnBuscarArticulos);
 		
 		JButton btnRealizarVenta = new JButton("Realizar Venta");
 		btnRealizarVenta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				int opcion =JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea realizar la venta?","Alerta!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-
-				if (opcion == 0){
-					try{
-						ReloadAll();
-						venta.setCliente(cliente);
-						venta.setCajero(Program.getLoggedUser());
-						venta.setTerminalVentas(0);
-						venta.setTienda(Program.getLoggedUser().getTienda());
-						venta.efectuar();
-						JOptionPane.showMessageDialog(null, "La venta se realizao correctamente!");
-					}catch(Exception e){
-					}
+				
+				if((venta.getEfectivoRecibido())>(venta.getTotal())){
 					
+					int opcion =JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea realizar la venta?","Alerta!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+					if (opcion == 0){
+						try{
+							ReloadAll();
+							venta.setCliente(cliente);
+							venta.setCajero(Program.getLoggedUser());
+							venta.setTerminalVentas(0);
+							venta.setTienda(Program.getLoggedUser().getTienda());
+							venta.efectuar();
+							JOptionPane.showMessageDialog(null, "La venta se realizao correctamente!");
+						}catch(Exception e){
+						}
+					}
 				}
+				else
+					JOptionPane.showMessageDialog(null, "El monto recibido no es suficiente para realizar la compra.", "Alerta!", JOptionPane.ERROR_MESSAGE);
 
 				
 			}
 		});
-		btnRealizarVenta.setBounds(282, 457, 127, 30);
+		btnRealizarVenta.setBounds(364, 457, 127, 30);
 		contentPane.add(btnRealizarVenta);
 		
 		JButton btnSalir = new JButton("Salir");
@@ -245,24 +254,80 @@ public class VentaFrame extends JFrame {
 				}
 			}
 		});
-		btnSalir.setBounds(419, 457, 127, 30);
+		btnSalir.setBounds(501, 457, 127, 30);
 		getContentPane().add(btnSalir);
 		
 		JLabel lblusarEnter = new JLabel("*Usar Enter");
 		lblusarEnter.setHorizontalAlignment(SwingConstants.LEFT);
-		lblusarEnter.setBounds(202, 321, 86, 14);
+		lblusarEnter.setBounds(234, 321, 86, 14);
 		contentPane.add(lblusarEnter);
 		
 		lblCliente = new JLabel("Cliente:");
 		lblCliente.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblCliente.setBounds(323, 14, 46, 14);
+		lblCliente.setBounds(402, 14, 46, 14);
 		contentPane.add(lblCliente);
 		
 		lblNota = new JLabel("*Para borrar un articulo use Delete.");
 		lblNota.setVerticalAlignment(SwingConstants.TOP);
 		lblNota.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNota.setBounds(349, 317, 197, 14);
+		lblNota.setBounds(383, 317, 245, 14);
 		contentPane.add(lblNota);
+		
+		JButton btnVaciarArticulos = new JButton("Vaciar articulos");
+		btnVaciarArticulos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				tabla.setModel(new DefaultTableModel(columnas, 0));
+				
+				for(Articulo art:AgregarProductoDialog.getArticulosSeleccionados()){
+					venta.retirarArticulo(art);
+				}
+				
+				AgregarProductoDialog.getArticulosSeleccionados().clear();
+				
+				tabla.getColumnModel().getColumn(0).setPreferredWidth(76);
+				tabla.getColumnModel().getColumn(1).setPreferredWidth(323);
+				ReloadAll();
+			}
+		});
+		btnVaciarArticulos.setBounds(489, 52, 139, 23);
+		contentPane.add(btnVaciarArticulos);
+		
+		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(txtAgregar.getText().length()>0){
+					List<Producto> tempProductos = new Producto().listar("WHERE p.codigo = '" + txtAgregar.getText() + "'");
+					
+					if(tempProductos.size()>0){
+						Producto producto = tempProductos.get(0);
+						
+						Articulo articuloTemp = new Articulo();
+						articuloTemp.setCantidad(1);
+						articuloTemp.setProducto(producto);
+						articuloTemp.setValor(producto.getPrecio());
+						articuloTemp.setTasaImpuestos(producto.getTasaImpuesto());
+						articuloTemp.totalizar();
+						
+						AgregarProductoDialog.getArticulosSeleccionados().add(articuloTemp);
+						venta.agregarArticulo(articuloTemp);
+						
+						ReloadAll();
+					}
+				}
+				
+			}
+		});
+		btnAgregar.setBounds(175, 52, 89, 23);
+		contentPane.add(btnAgregar);
+		
+		txtAgregar = new JTextField();
+		txtAgregar.setBounds(79, 50, 86, 25);
+		contentPane.add(txtAgregar);
+		txtAgregar.setColumns(10);
+		
+		JLabel lblCodigo = new JLabel("Codigo:");
+		lblCodigo.setBounds(20, 51, 58, 25);
+		contentPane.add(lblCodigo);
 		
 		
 		//////Para eliminar articulos///////
@@ -276,20 +341,24 @@ public class VentaFrame extends JFrame {
 		  actionMap.put(DELETE, new AbstractAction() {
 		     public void actionPerformed(ActionEvent e) {
 		    	 
-		    	 Articulo articuloABorrar= new Articulo ();
-		    	 String productoDescripcion = (String) tabla.getValueAt(tabla.getSelectedRow(), 1);
+		    	 if(tabla.getSelectedRow() >=0){
 		    	 
-		         
-		         String[] campos =  {"producto"};
-		         Object[][] datos = Utilidades.listToBidiArray(venta.getArticulos(), campos);
-		 		
-		 		for(int i = 0; i< datos.length; i++){
-		 			if (productoDescripcion == venta.getArticulos().get(i).getProducto().getDescripcion())
-		 				articuloABorrar = venta.getArticulos().get(i);
-		 		}
-		 		
-		 		venta.retirarArticulo(articuloABorrar);
-		 		ReloadAll();
+			    	 Articulo articuloABorrar= new Articulo ();
+			    	 String productoDescripcion = (String) tabla.getValueAt(tabla.getSelectedRow(), 1);
+			    	 
+			         
+			         String[] campos =  {"producto"};
+			         Object[][] datos = Utilidades.listToBidiArray(AgregarProductoDialog.getArticulosSeleccionados(), campos);
+			 		
+			 		for(int i = 0; i< datos.length; i++){
+			 			if (productoDescripcion == AgregarProductoDialog.getArticulosSeleccionados().get(i).getProducto().getDescripcion())
+			 				articuloABorrar = AgregarProductoDialog.getArticulosSeleccionados().get(i);
+			 		}
+			 		
+			 		AgregarProductoDialog.getArticulosSeleccionados().remove(articuloABorrar);
+			 		venta.retirarArticulo(articuloABorrar);
+			 		ReloadAll();
+		    	 }
 		     }
 		  });
 
