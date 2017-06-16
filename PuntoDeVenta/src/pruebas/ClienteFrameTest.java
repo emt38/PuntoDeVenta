605b6cc5580bc614e18dd09519f3aa72b6711e37
Jose Miguel <remigio9966@gmail.com>
2017-06-16 01:03:40 -0400

    ClienteFrameTest Listo

--- PuntoDeVenta/src/pruebas/ClienteFrameTest.java
+++ PuntoDeVenta/src/pruebas/ClienteFrameTest.java
@@ -10,25 +10,32 @@ import modelos.Pais;
 import principal.RobotFingers;
 import vistas.CiudadesFrame;
 import vistas.ClienteFrame;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.event.KeyEvent;
 
-public class ClienteFrameTest {
+public class ClienteFrameTest  {
 	private int velocity = 4;
 	private int lag = 1000;
 	@Test
-	public void InsertTest() {
-		
+	public void InsertTest()throws IOException {
+		 RobotFingers rob = new RobotFingers();
 		ClienteFrame frameCliente = new ClienteFrame();	
 		frameCliente.setLocationRelativeTo(null);
 		frameCliente.setResizable(false);
 		frameCliente.setVisible(true);
-	
 		
-        RobotFingers rob = new RobotFingers();
-        
-       
         rob.moveToComponentAnimated(frameCliente.btnNuevo, velocity);
 		rob.leftClickComponent(frameCliente.btnNuevo);
 		rob.delay(lag * 2);
+
 		
 		rob.moveToComponentAnimated(frameCliente.txtNombre, velocity);
 		rob.leftClickComponent((frameCliente.txtNombre));
@@ -37,29 +44,29 @@ public class ClienteFrameTest {
 		rob.delay(lag);
 		
 		rob.moveToComponentAnimated(frameCliente.txtApellido, velocity);
-		rob.leftClickComponent(frameCliente.txtApellido);
+		rob.leftClickComponentCenter(frameCliente.txtApellido);
 		rob.delay(lag);
 		rob.writeString("Apellido de prueba");
 		rob.delay(lag);
 		
 		rob.moveToComponentAnimated(frameCliente.txtDireccion, velocity);
-		rob.leftClickComponent(frameCliente.txtDireccion);
+		rob.leftClickComponentCenter(frameCliente.txtDireccion);
 		rob.delay(lag);
 		rob.writeString("Yo vivo lejos");
 		rob.delay(lag);
 		
 		rob.moveToComponentAnimated(frameCliente.txtCelular, velocity);
-		rob.leftClickComponent(frameCliente.txtCelular);
+		rob.leftClickComponentCenter(frameCliente.txtCelular);
 		rob.delay(lag);
 		rob.writeString("809-111-1111");
 		rob.delay(lag);
 		rob.moveToComponentAnimated(frameCliente.txtIdentificacion, velocity);
-		rob.leftClickComponent(frameCliente.txtIdentificacion);
+		rob.leftClickComponentCenter(frameCliente.txtIdentificacion);
 		rob.delay(lag);
 		rob.writeString("402-2463259-2");
 		rob.delay(lag);
 		rob.moveToComponentAnimated(frameCliente.cmbSexo, velocity);
-		rob.leftClickComponent(frameCliente.cmbSexo);
+		rob.leftClickComponentCenter(frameCliente.cmbSexo);
 		rob.delay(lag);
 		rob.writeString("Masculino");
 		rob.delay(lag);
@@ -67,7 +74,7 @@ public class ClienteFrameTest {
 		rob.leftClick(650,385);
 		
 		rob.moveToComponentAnimated(frameCliente.btnGuardar, velocity);
-		rob.leftClickComponent(frameCliente.btnGuardar);
+		rob.leftClickComponentCenter(frameCliente.btnGuardar);
 		rob.delay(lag);
 		
 	}
@@ -83,39 +90,39 @@ public class ClienteFrameTest {
         
        
         rob.moveToComponentAnimated(frameCliente.btnModificar, velocity);
-		rob.leftClickComponent(frameCliente.btnModificar);
+		rob.leftClickComponentCenter(frameCliente.btnModificar);
 		rob.delay(lag * 2);
 		
 		rob.moveToComponentAnimated(frameCliente.txtNombre, velocity);
-		rob.leftClickComponent((frameCliente.txtNombre));
+		rob.leftClickComponentCenter((frameCliente.txtNombre));
 		rob.delay(lag);
 		rob.writeString("Nombre de prueba");
 		rob.delay(lag);
 		
 		rob.moveToComponentAnimated(frameCliente.txtApellido, velocity);
-		rob.leftClickComponent(frameCliente.txtApellido);
+		rob.leftClickComponentCenter(frameCliente.txtApellido);
 		rob.delay(lag);
 		rob.writeString("Apellido de prueba");
 		rob.delay(lag);
 		
 		rob.moveToComponentAnimated(frameCliente.txtDireccion, velocity);
-		rob.leftClickComponent(frameCliente.txtDireccion);
+		rob.leftClickComponentCenter(frameCliente.txtDireccion);
 		rob.delay(lag);
 		rob.writeString("Yo vivo lejos");
 		rob.delay(lag);
 		
 		rob.moveToComponentAnimated(frameCliente.txtCelular, velocity);
-		rob.leftClickComponent(frameCliente.txtCelular);
+		rob.leftClickComponentCenter(frameCliente.txtCelular);
 		rob.delay(lag);
 		rob.writeString("809-111-1111");
 		rob.delay(lag);
 		rob.moveToComponentAnimated(frameCliente.txtIdentificacion, velocity);
-		rob.leftClickComponent(frameCliente.txtIdentificacion);
+		rob.leftClickComponentCenter(frameCliente.txtIdentificacion);
 		rob.delay(lag);
 		rob.writeString("402-2463259-2");
 		rob.delay(lag);
 		rob.moveToComponentAnimated(frameCliente.cmbSexo, velocity);
-		rob.leftClickComponent(frameCliente.cmbSexo);
+		rob.leftClickComponentCenter(frameCliente.cmbSexo);
 		rob.delay(lag);
 		rob.writeString("Masculino");
 		rob.delay(lag);
@@ -123,7 +130,7 @@ public class ClienteFrameTest {
 		rob.leftClick(650,385);
 		
 		rob.moveToComponentAnimated(frameCliente.btnGuardar, velocity);
-		rob.leftClickComponent(frameCliente.btnGuardar);
+		rob.leftClickComponentCenter(frameCliente.btnGuardar);
 		rob.delay(lag);
 	}
 
